ef157131fc13a8d6065854a2a2369039f902c2dd
Voroshilov Maksim <voroshilovmax90@gmail.com>
2025-09-01 18:43:48 +0200

    done

--- index.js
+++ index.js
@@ -4,24 +4,49 @@ const jsonfile = require("jsonfile");
 const moment = require("moment");
 const random = require("random");
 
+// Change the working directory to where your local repository is located
+const git = simpleGit("/Users/voroshilovmax/Development/node/microservices");
+
 const makeCommit = (n) => {
-  if (n === 0) return simpleGit().push();
+  if (n === 0) {
+    // Push changes to the remote repository
+    git.push(["-u", "origin", "master"], (err, result) => {
+      if (err) {
+        console.error("Error pushing to remote:", err);
+      } else {
+        console.log("Pushed changes to remote repository");
+      }
+    });
+    return;
+  }
+
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const DATE = moment()
-    .subtract(1, "y")
+    .subtract(0, "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
     .format();
+
   const data = {
     date: DATE,
   };
   console.log(DATE);
+
   jsonfile.writeFile(FILE_PATH, data, () => {
-    simpleGit()
+    git
       .add([FILE_PATH])
-      .commit(DATE, { "-date": DATE }, makeCommit.bind(this, --n));
+      .commit(DATE, { "--date": DATE })
+      .push(["-u", "origin", "origin"], (err, result) => {
+        if (err) {
+          console.error("Error pushing to remote:", err);
+        } else {
+          console.log("Pushed changes to remote repository");
+          makeCommit(--n);
+        }
+      });
   });
 };
+
 makeCommit(500);
